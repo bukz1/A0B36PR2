89c915293481ab35e867f689ce9b0e615c9f275c
Zdenek Buk <bukz1@fel.cvut.cz>
2013-02-14 11:46:25 +0100

    Drobna uprava vypisu

--- Semestralni prace/src/semestralni/prace/SemestralniPrace.java
+++ Semestralni prace/src/semestralni/prace/SemestralniPrace.java
@@ -14,6 +14,6 @@ public class SemestralniPrace {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!!!");
     }
 }
